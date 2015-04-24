2023ed4c2761343c08f77c8fc1c1b275391bc9ac
GrantChung <ghill.ghill@gmail.com>
2015-04-24 07:45:06 +0000

    fix

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,15 @@ import 'package:chrome/chrome_app.dart' as chrome;
 import 'package:chrome/gen/runtime.dart';
 import 'package:chrome/gen/serial.dart';
 
-void main() {
-  
+var msgField = querySelector("#msg");
+var selectport = querySelector("#selectPort");
+
+void main() {  
+  addMsg("Init...");
+  addMsg(selectport.selectedIndex);
 }
+
+void addMsg(String str) {
+  msgField.appendHtml(str);
+}
+
